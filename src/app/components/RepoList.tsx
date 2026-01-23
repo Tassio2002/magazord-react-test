b5a757cebaaed0d1db27fd27c138998932c9cbf0
Tassio <81255475+Tassio2002@users.noreply.github.com>
2026-01-23 00:20:39 -0300

    feat: repo list component done

--- src/app/components/RepoList.tsx
+++ src/app/components/RepoList.tsx
@@ -1,11 +1,34 @@
-import { Repos } from "@/types/Repos";
+import RepoItem from "./RepoItem";
+import { useQuery } from "@tanstack/react-query";
 
-export default function RepoList({
-    ownerLogin,
-    name,
-    description,
-    starsQuantity,
-    forksQuantity
-}: Repos) {
-    
+function fetcher(url: string) {
+    return fetch(url).then((res) => res.json());
 }
+export default function RepoList() {
+    const repos = useQuery({
+        queryKey: ["repos"],
+        queryFn: () => fetcher("https://api.github.com/users/Tassio2002/repos")
+    });
+
+    if (repos.isPending) return <div>Loading...</div>;
+
+    if (repos?.data?.error) return <div>Error loading repository list.</div>
+
+    if (repos?.data?.length === 0) return <div>No repository was found.</div>
+    return (
+        <ul className="flex flex-col gap-12">
+            {repos.data?.map((repo: any) => ( // tipar corretamente
+                <li key={repo.id}>
+                    <RepoItem
+                        ownerLogin={repo.owner.login}
+                        name={repo.name}
+                        description={repo.description}
+                        starsQuantity={repo.stargazers_count}
+                        forksQuantity={repo.forks_count}
+                    />
+                </li>
+            ))}
+        </ul>
+    )
+}
+
--- src/app/components/repos.tsx
+++ src/app/components/repos.tsx
@@ -1,44 +1,15 @@
 'use client';
 
-import { useQuery } from "@tanstack/react-query";
 import Tabs from "./Tabs";
 import FilterSection from "./FilterSection";
-import RepoItem from "./RepoItem";
-
-function fetcher(url: string) {
-    return fetch(url).then((res) => res.json());
-}
+import RepoList from "./RepoList";
 
 export default function Repos() {
-    const repos = useQuery({
-        queryKey: ["repos"],
-        queryFn: () => fetcher("https://api.github.com/users/Tassio2002/repos")
-    });
-
-    if (repos.isPending) return <div>Loading...</div>;
-
-    if (repos?.data?.error) return <div>Error loading repository list.</div>
-
-    if (repos?.data?.length === 0) return <div>No repository was found.</div>
-
     return (
         <div>
             <Tabs />
             <FilterSection />
-            {/*transformar em componente */}
-            <ul className="flex flex-col gap-12"> 
-                {repos.data?.map((repo: any) => ( // tipar corretamente
-                    <li key={repo.id}>
-                        <RepoItem 
-                            ownerLogin={repo.owner.login}
-                            name={repo.name}
-                            description={repo.description}
-                            starsQuantity={repo.stargazers_count}
-                            forksQuantity={repo.forks_count}
-                        />
-                    </li>
-                ))}
-            </ul>
+            <RepoList />
         </div>
     )
 }
